a574cad972a765e49d0d70f292cc42e469d5ec51
GogeH <skibadee@inbox.ru>
2021-11-29 20:05:38 +0300

    Попытка 3

---
+++ project/src/components/user-block-logged/user-block-logged.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { createMockAuthInfo } from '../../mocks/authorizationFake';
+import { Provider } from 'react-redux';
+import { configureMockStore } from '@jedmao/redux-mock-store';
+import UserBlockLogged from './user-block-logged';
+import { AppRoute } from '../../types/enum';
+
+const history = createMemoryHistory();
+const mockStore = configureMockStore();
+const store = mockStore({
+  'USER_AUTH': {authInfo: createMockAuthInfo()},
+});
+
+describe('Component: UserBlockLogged', () => {
+  it('should render correctly', () => {
+    const { container } = render(
+      <Provider store={store}>
+        <Router history={history}>
+          <UserBlockLogged />
+        </Router>
+      </Provider>,
+    );
+
+    expect(screen.getAllByRole('link')[0].getAttribute('href')).toEqual(AppRoute.MyList);
+    expect(screen.getAllByRole('link')[1].getAttribute('href')).toEqual('/');
+
+    expect(container.querySelector('.user-block__avatar')).toBeInTheDocument();
+    expect(container.querySelector('.user-block__item')).toBeInTheDocument();
+  });
+});
